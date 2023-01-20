edcaa38cddfa697b3019d947a1397d3fb4c44692
ilya.kukin <ilya.kukin@taskdata.org>
2023-01-20 21:47:05 +0300

    add about app and setup about url

--- opsocom/opsocom/urls.py
+++ opsocom/opsocom/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from about.views import index
+from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('about/', include('about.urls'))
 ]
